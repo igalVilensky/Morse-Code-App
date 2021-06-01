0456a500a5f46bd2c91c0dc1195e76bb3cb91a0e
igalv <vilenskyigal@gmail.com>
2021-06-01 15:24:23 +0200

    handle translate function

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,62 +2,79 @@ import React, { useState } from "react";
 
 function TextsField() {
   const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const alphabet = {
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+    " ": "/",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    0: "-----",
+  };
+
+  const handleTranslate = (str) => {
+    let strToArr = str.toLowerCase().split("");
+
+    console.log(strToArr);
+
+    let something = [];
+
+    strToArr.forEach((element) => something.push(alphabet[element]));
+
+    console.log(something);
+
+    setResult(something.join(""));
+  };
+
+  // Handle Change
 
   const handleChange = (e) => {
-    e.preventDefault();
+    setInput(e.target.value);
   };
-
-  const morseChars = {
-    "-----": "0",
-    ".----": "1",
-    "..---": "2",
-    "...--": "3",
-    "....-": "4",
-    ".....": "5",
-    "-....": "6",
-    "--...": "7",
-    "---..": "8",
-    "----.": "9",
-    ".-": "a",
-    "-...": "b",
-    "-.-.": "c",
-    "-..": "d",
-    ".": "e",
-    "..-.": "f",
-    "--.": "g",
-    "....": "h",
-    "..": "i",
-    ".---": "j",
-    "-.-": "k",
-    ".-..": "l",
-    "--": "m",
-    "-.": "n",
-    "---": "o",
-    ".--.": "p",
-    "--.-": "q",
-    ".-.": "r",
-    "...": "s",
-    "-": "t",
-    "..-": "u",
-    "...-": "v",
-    ".--": "w",
-    "-..-": "x",
-    "-.--": "y",
-    "--..": "z",
-    "-.-.--": "!",
-    ".-.-.-": ".",
-    "--..--": ",",
-  };
-
-  console.log(Object.entries(morseChars));
 
   return (
     <div>
-      <form onSubmit={handleChange}>
-        <input type="text" placeholder="Enter your text here"></input>
-        <button className="submit-button">Translate</button>
-      </form>
-      <div className="result">some text</div>
+      <input
+        type="text"
+        placeholder="Enter your text here"
+        onChange={handleChange}
+      ></input>
+      <button className="submit-button" onClick={() => handleTranslate(input)}>
+        Translate
+      </button>
+
+      <div className="result">{result}</div>
     </div>
   );
 }
