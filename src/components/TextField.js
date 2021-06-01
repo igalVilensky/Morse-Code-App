9d2ed3a30621c874a70ac200efec6f214940d312
igalv <vilenskyigal@gmail.com>
2021-06-01 11:44:20 +0200

    morse chars object

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -6,11 +6,59 @@ function TextsField() {
   const handleChange = (e) => {
     e.preventDefault();
   };
+
+  const morseChars = {
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    "-.-.--": "!",
+    ".-.-.-": ".",
+    "--..--": ",",
+  };
+
+  console.log(Object.entries(morseChars));
+
   return (
-    <form onSubmit={handleChange}>
-      <input type="text" placeholder="Enter your text here"></input>
-      <button className="submit-button">Translate</button>
-    </form>
+    <div>
+      <form onSubmit={handleChange}>
+        <input type="text" placeholder="Enter your text here"></input>
+        <button className="submit-button">Translate</button>
+      </form>
+      <div className="result">some text</div>
+    </div>
   );
 }
 
